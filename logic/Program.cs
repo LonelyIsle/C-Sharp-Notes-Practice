7978a5848d99a438e682f46b349c0f4575868c49
William <d.j.stewart7873@gmail.com>
2026-06-24 09:51:02 -0500

    started the setup for the next section

---
+++ logic/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boolean Data Types
+            
+            
+        }
+    }
+}
