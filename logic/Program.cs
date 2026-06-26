2d8a4aa24c205d0d9a2676e295281132c60755d3
William <d.j.stewart7873@gmail.com>
2026-06-26 09:27:22 -0500

    update for this mornings learnings

--- logic/Program.cs
+++ logic/Program.cs
@@ -7,6 +7,26 @@ namespace logic
         static void Main(string[] args)
         {
             //Boolean Data Types
+            //True or False: The number 500 is greater than 24.
+            bool answerOne = true;
+
+            //True or False: “coffee” contains the letter “a”.
+            bool answerTwo = false;
+
+            //Equality operators
+            //Cookie store example
+            int cookiesSoldGuess = 250;
+
+            int storeOneCookies = 110;
+            int storeTwoCookies = 135;
+
+            int cookiesSoldActual = storeOneCookies + storeTwoCookies;
+
+            bool guessedCorrect = (cookiesSoldGuess == cookiesSoldActual);
+
+            Console.WriteLine(guessedCorrect);
+
+            //Comparison operators
             
             
         }
