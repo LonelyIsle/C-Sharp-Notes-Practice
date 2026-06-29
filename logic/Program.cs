5c1621d27193e3d6a8f02dfe561764eb0adc08c2
William <d.j.stewart7873@gmail.com>
2026-06-29 11:41:17 -0500

    finished the boolean logic and conditionals

--- logic/Program.cs
+++ logic/Program.cs
@@ -27,7 +27,29 @@ namespace logic
             Console.WriteLine(guessedCorrect);
 
             //Comparison operators
-            
+            double timeToDinner = 4;
+
+            double distance = 95;
+            double rate = 30;
+
+            double tripDuration = distance / rate;
+
+            bool answer = tripDuration <= timeToDinner;
+
+            Console.WriteLine(answer);
+
+            //Logical operators
+            //AND, OR, NOT
+            bool beach = true;
+            bool hiking = false;
+            bool city = true;
+
+            bool yourNeeds = (beach && city);
+
+            bool friendNeeds = (beach || hiking);
+
+            bool tripDecision = (yourNeeds && friendNeeds);
+            Console.WriteLine(tripDecision);
             
         }
     }
