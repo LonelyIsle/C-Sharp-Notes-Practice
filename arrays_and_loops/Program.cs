72d08d57146e05f9277b5651dc31d5297b4a93cd
William <d.j.stewart7873@gmail.com>
2026-07-14 15:29:02 -0500

    did the first problem

--- arrays_and_loops/Program.cs
+++ arrays_and_loops/Program.cs
@@ -6,9 +6,25 @@ namespace arrays_and_loops
     {
         static void Main(string[] args)
         {
-            //Your code here
+            //Declaration and initilization of variables
+            string[] summerStrut;
+            int[] ratings;
+
+            summerStrut = new string[] {"Juice","Missing U","Raspberry Beret","New York Groove","Make Me Feel","Rebel Rebel","Despacito","Los Angeles"};
+
+            ratings = new int[]{1,2,3,4,5,4,3,2};
             
+            //implicit typed arrays, they are typed differently and the data type is implied by the elements in the arrays
+            /*
+            var summerStrut = new[] { "Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Me Feel", "Rebel Rebel", "Despacito", "Los Angeles" };
+
+            var ratings = new[] { 5, 4, 4, 3, 3, 5, 5, 4 };
+            */
+
+            //Array Length
+            /*
             
+            */
         }
     }
 }
